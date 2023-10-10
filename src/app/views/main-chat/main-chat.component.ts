abd047c27b8aea5e3301cd4aecbab2f6a917d9d0
micapiacenza <micapiacenza219@gmail.com>
2023-10-10 19:36:21 +1000

    Fully working version of chat changin

--- src/app/views/main-chat/main-chat.component.ts
+++ src/app/views/main-chat/main-chat.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit, ElementRef, ViewChild, SimpleChanges} from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { SocketioService } from '../../common/services/socket/socketio.service';
-import {MessageInterface} from "../../common/interfaces/message.interface";
 
 @Component({
   selector: 'app-main-chat',
@@ -19,7 +18,7 @@ export class MainChatComponent implements OnInit {
   constructor(private socketioService: SocketioService) {}
 
   ngOnInit() {
-    this.socketioService.getMessages().subscribe((messageData: { message: string; room: string }) => {
+    this.socketioService.getMessages().subscribe((messageData: any) => {
       console.log('Received message component:', messageData);
       this.messages.push(messageData);
     });
@@ -32,7 +31,7 @@ export class MainChatComponent implements OnInit {
     }
   }
 
-  filterChatMessages(): string[] {
+  filterChatMessages(): any[] {
     return this.messages.filter((message) => {
       return message.room === this.selectedRoom;
     });
